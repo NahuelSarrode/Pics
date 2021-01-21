ac99e2815e50da79003eb6758436a30ae897a3f4
LordNahuel <nahuelsarrode@gmail.com>
2021-01-21 17:00:14 -0300

    searchBar component working

---
+++ src/components/SearchBar.js
@@ -0,0 +1,32 @@
+import React, { Component } from "react";
+
+class SearchBar extends Component {
+  state = { term: '' }
+
+  onSubmit = (event) => {
+    event.preventDefault();
+
+    console.log(this.state.term);
+  }
+
+  render() {
+    return (
+      <div className="ui segment">
+        <form className="ui form" onSubmit={this.onSubmit}>
+          <div className="field">
+            <label>Image Search</label>
+            <input 
+              type="text" 
+              placeholder="Insert an pic name" 
+              required
+              value={this.state.term}
+              onChange={(e) => this.setState({ term: e.target.value })}
+            />
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default SearchBar;
